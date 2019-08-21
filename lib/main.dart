38f02fbf68f6e3e3ef48d679f8c1a3a1436fa6fe
Marco <ma.monzon@soprasteria.com>
2019-08-21 11:57:00 +0200

    feat: questions and answers are now in a Question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,21 +27,18 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List<bool> answers = [
-    false,
-    true,
-    true,
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
   ];
   int questionNumber = 0;
 
   void nextQuestion(bool userAnswer) {
-    if (questionNumber < questions.length) {
-      if (userAnswer == answers[questionNumber]) {
+    if (questionNumber < questionBank.length) {
+      if (userAnswer == questionBank[questionNumber].questionAnswer) {
         scoreKeeper.add(
           Icon(
             Icons.check,
@@ -73,9 +71,9 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionNumber < questions.length
-                    ? questions[questionNumber]
-                    : questions[questionNumber - 1],
+                questionNumber < questionBank.length
+                    ? questionBank[questionNumber].questionText
+                    : questionBank[questionNumber - 1].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -131,9 +129,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
