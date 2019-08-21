b8700e9d6ae00a0c948fb9ca4ca1f6ddac768f2b
Marco <ma.monzon@soprasteria.com>
2019-08-21 11:28:07 +0200

    feat: evaluate answers ans display if wrong or right

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,42 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+  int questionNumber = 0;
+
+  void nextQuestion(bool userAnswer) {
+    if (questionNumber < questions.length) {
+      if (userAnswer == answers[questionNumber]) {
+        scoreKeeper.add(
+          Icon(
+            Icons.check,
+            color: Colors.green,
+          ),
+        );
+      } else {
+        scoreKeeper.add(
+          Icon(
+            Icons.close,
+            color: Colors.red,
+          ),
+        );
+      }
+      setState(() {
+        questionNumber++;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +73,9 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questionNumber < questions.length
+                    ? questions[questionNumber]
+                    : questions[questionNumber - 1],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +100,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                nextQuestion(true);
               },
             ),
           ),
@@ -80,11 +119,14 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                nextQuestion(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
