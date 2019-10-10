f0e42f16fdebec868468d7ec3bf1c7b1b36082fd
Marco <marco238cuba@gmail.com>
2019-10-10 16:36:41 +0200

    feat: results and restart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,11 +29,15 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
   int questionNumber = 0;
+  int rightAnswers = 0;
+  int wrongAnswers = 0;
   QuizBrain quizBrain = QuizBrain();
+  int questionsLenght;
 
-  void nextQuestion(bool userAnswer) {
-    if (questionNumber < quizBrain.questionBank.length) {
-      if (userAnswer == quizBrain.questionBank[questionNumber].questionAnswer) {
+  void checkQuestion(bool userAnswer) {
+    if (questionNumber < quizBrain.getQuestionsLenght()) {
+      if (userAnswer == quizBrain.getQuestionAnswer(questionNumber)) {
+        rightAnswers++;
         scoreKeeper.add(
           Icon(
             Icons.check,
@@ -40,6 +45,7 @@ class _QuizPageState extends State<QuizPage> {
           ),
         );
       } else {
+        wrongAnswers++;
         scoreKeeper.add(
           Icon(
             Icons.close,
@@ -51,6 +57,111 @@ class _QuizPageState extends State<QuizPage> {
         questionNumber++;
       });
     }
+  }
+
+  _onAlertWithStylePressed(context) {
+    var alertStyle = AlertStyle(
+        animationType: AnimationType.fromTop,
+        isCloseButton: false,
+        isOverlayTapDismiss: false,
+        animationDuration: Duration(milliseconds: 200),
+        alertBorder: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4.0),
+          side: BorderSide(
+            color: Colors.grey,
+          ),
+        ),
+        titleStyle: rightAnswers >= wrongAnswers
+            ? TextStyle(color: Color.fromRGBO(114, 224, 128, 1.0), fontSize: 24)
+            : TextStyle(color: Colors.red, fontSize: 24),
+        descStyle: TextStyle(
+          color: Colors.grey[600],
+          fontWeight: FontWeight.w400,
+        ));
+
+    questionsLenght = quizBrain.getQuestionsLenght();
+
+    Alert(
+      context: context,
+      style: alertStyle,
+      type: rightAnswers >= wrongAnswers ? AlertType.success : AlertType.error,
+      title: rightAnswers >= wrongAnswers ? "QUIZ PASSED!" : "QUIZ FAILED!",
+      content: Container(
+        height: 60,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Right Answers',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(114, 224, 128, 1.0),
+                  ),
+                ),
+                Text(
+                  '$rightAnswers/$questionsLenght',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Color.fromRGBO(114, 224, 128, 1.0),
+                  ),
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Wrong Answers',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                Text(
+                  '$wrongAnswers/$questionsLenght',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+      desc: "This quiz has ended. Check your stats:",
+      buttons: [
+        DialogButton(
+          child: Text(
+            "RESTART",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          onPressed: () {
+            _restartApp();
+            Navigator.pop(context);
+          },
+          gradient: LinearGradient(colors: [
+            Colors.greenAccent,
+            Colors.redAccent,
+          ]),
+          radius: BorderRadius.circular(4.0),
+        ),
+      ],
+    ).show();
+  }
+
+  _restartApp() {
+    setState(() {
+      scoreKeeper = [];
+      questionNumber = 0;
+      rightAnswers = 0;
+      wrongAnswers = 0;
+    });
   }
 
   @override
@@ -65,9 +176,9 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionNumber < quizBrain.questionBank.length
-                    ? quizBrain.questionBank[questionNumber].questionText
-                    : quizBrain.questionBank[questionNumber - 1].questionText,
+                questionNumber < quizBrain.getQuestionsLenght()
+                    ? quizBrain.getQuestionText(questionNumber)
+                    : quizBrain.getQuestionText(questionNumber - 1),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -77,45 +188,69 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+        questionNumber < quizBrain.getQuestionsLenght() - 1
+            ? Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: FlatButton(
+                    textColor: Colors.white,
+                    color: Colors.green,
+                    child: Text(
+                      'True',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                    onPressed: () {
+                      //The user picked true.
+                      checkQuestion(true);
+                    },
+                  ),
+                ),
+              )
+            : new Container(),
+        questionNumber < quizBrain.getQuestionsLenght() - 1
+            ? Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: FlatButton(
+                    color: Colors.red,
+                    child: Text(
+                      'False',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      //The user picked false.
+                      checkQuestion(false);
+                    },
+                  ),
+                ),
+              )
+            : Expanded(
+                flex: 0,
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: FlatButton(
+                    color: Colors.blue,
+                    child: Text(
+                      'Finish Quiz!',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    onPressed: () {
+                      _onAlertWithStylePressed(context);
+                    },
+                  ),
                 ),
               ),
-              onPressed: () {
-                //The user picked true.
-                nextQuestion(true);
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                //The user picked false.
-                nextQuestion(false);
-              },
-            ),
-          ),
-        ),
         Container(
           height: 30.0,
           child: Row(
